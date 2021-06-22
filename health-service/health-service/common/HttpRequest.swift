de49a657d71211a2b7edcab82fc27617295b8246
moonseo.kim <moonseo.kim@nhn.com>
2021-06-22 21:10:58 +0900

    develop http communication

--- health-service/health-service/common/HttpRequest.swift
+++ health-service/health-service/common/HttpRequest.swift
@@ -7,11 +7,77 @@ struct ResponseHeader: Codable {
 }
 
 func requestGet(url: String, completionHandler: @escaping (Bool, Any) -> Void) {
+    guard let url = URL(string: url) else {
+        print("Error: cannot create URL")
+        return
+    }
     
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        guard error == nil else {
+            print("Error: error calling GET")
+            print(error!)
+            return
+        }
+        
+        guard let data = data else {
+            print("Error: Did not receive data")
+            return
+        }
+        
+        guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
+            print("Error: HTTP request failed")
+            return
+        }
+        
+        guard let output = try? JSONDecoder().decode(ResponseHeader.self, from: data) else {
+            print("Error: JSON Data Parsing failed")
+            return
+        }
+        
+        completionHandler(true, output.result)
+    }.resume()
 }
 
 func requestPost(url: String, method: String, param: [String: Any], completionHandler: @escaping (Bool, Any) -> Void) {
+    let sendData = try! JSONSerialization.data(withJSONObject: param, options: [])
     
+    guard let url = URL(string: url) else {
+        print("Error: cannot create URL")
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = method
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = sendData
+    
+    URLSession.shared.dataTask(with: request) { (data, response, error) in
+        guard error == nil else {
+            print("Error: error calling " + method)
+            print(error!)
+            return
+        }
+        
+        guard let data = data else {
+            print("Error: Did not receive data")
+            return
+        }
+        
+        guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
+            print("Error: HTTP request failed")
+            return
+        }
+        
+        guard let output = try? JSONDecoder().decode(ResponseHeader.self, from: data) else {
+            print("Error: JSON Data Parsing failed")
+            return
+        }
+        
+        completionHandler(true, output.result)
+    }.resume()
 }
 
 func request(_ url: String, _ method: String, _ param: [String: Any]? = nil, completionHandler: @escaping (Bool, Any) -> Void) {
